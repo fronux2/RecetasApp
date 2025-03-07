30aa896f77de4a44b2cec9609c5f2ce30cd51027
marcos <marcosantonio.munozg@gmail.com>
2025-03-06 23:03:40 -0300

    barra buscador listCard3

--- components/cards/ListCard3.tsx
+++ components/cards/ListCard3.tsx
@@ -2,25 +2,43 @@ import { useEffect, useState } from 'react';
 import Card from './Card';
 import { fetchRecipes } from '../../services/recipeService';
 import { Recipe } from '../../types/models';
-import { FlatList, View } from 'react-native';
+import { FlatList, TextInput, View } from 'react-native';
 import { Link } from 'expo-router';
 const ListCard3 = () => {
   const [recipes, setRecipes] = useState<Recipe[]>([]);
+  const [buscador, setBuscador] = useState<string>('');
+  const [filteredRecipes, setFilteredRecipes] = useState<Recipe[]>([]);
   useEffect(() => {
     const fetchData = async () => {
       try {
         const data = await fetchRecipes();
         setRecipes(data);
+        setFilteredRecipes(data);
       } catch (error) {
         console.log(error);
       }
     };
     fetchData();
   }, []);
+
+  const handleSearch = (value: string) => {
+    setBuscador(value);
+    const filterRecipes = recipes.filter((recipe) =>
+      recipe.title.toLowerCase().includes(value.toLowerCase())
+    );
+    setFilteredRecipes(filterRecipes);
+  };
+
   return (
     <View className="flex-1 ">
+      <TextInput
+        value={buscador}
+        onChangeText={handleSearch}
+        placeholder="Buscar"
+        className="w-full p-3 mb-4 border rounded-lg bg-white text-gray-700"
+      />
       <FlatList
-        data={recipes}
+        data={filteredRecipes}
         renderItem={({ item }) => (
           <Link key={item.id} href={`/${item.id}`}>
             <Card
